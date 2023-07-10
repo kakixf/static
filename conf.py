898d0a1bd3718124456366f6c60467ad6c43e0eb
wangxiaofei <wangxiaofei@tianyancha.com>
2023-07-10 16:18:14 +0800

    update

---
+++ conf.py
@@ -0,0 +1,26 @@
+source_list = [
+    {
+        "name": "meotv",
+        "url": "http://miaotvs.cn/meow",
+        "allow_redirect": True,
+        "desc": "猫影视tv",
+        "from": "api"
+    }, {
+        "name": "feimao",
+        "url": "http://我不是.肥猫.love:63",
+        "allow_redirect": False,
+        "desc": "肥猫",
+        "from": "api"
+    }, {
+        "name": "tvbox",
+        "url": "https://github.com/kakixf/TVBox-1/blob/main/tvbox.json",
+        "allow_redirect": False,
+        "desc": "默认",
+        "from": "git"
+    }, {
+        "name": "xiaoya",
+        "url": "https://github.com/kakixf/TVBox-1/blob/main/xiaoya.json",
+        "allow_redirect": False,
+        "desc": "小雅",
+        "from": "git"
+    }]
--- update_source.py
+++ update_source.py
@@ -1,38 +1,73 @@
-# 从喵影视更新数据源
+# coding=utf-8
+# 从数据源同步到本地
 
 import requests
 import json
-
-url = "http://miaotvs.cn/meow"
+from conf import source_list
 
 headers = {
     'User-Agent': 'okhttp/3.12.11'
 }
 
 
-def get_redirect():
+def get_redirect(url):
     res = requests.get(url, headers=headers, allow_redirects=False)
     return res.headers["Location"]
 
 
-def get_conf():
-    host = get_redirect()
-    response = requests.get(host, headers=headers)
-    json_text = response.text
+def get_conf(url):
+    response = requests.get(url, headers=headers)
+    json_text = response.content.decode("utf-8")
     res = ""
     for line in json_text.split("\r\n"):
         if not line.strip().startswith("//"):
             res += line.strip()
-    res = json.dumps(json.loads(res), indent=4, ensure_ascii=False)
-    print(res)
+    try:
+        res_json = json.loads(res)
+        remove_keys(res_json)
+        res = json.dumps(res_json, indent=4, ensure_ascii=False)
+        # print(res)
+        return res
+    except Exception as e:
+        print("json 解析失败", e)
+        return ""
+
+
+def get_git_conf(url):
+    response = requests.get(url)
+    res = response.json()
+    res_json = json.loads(res["payload"]["blob"]["rawBlob"])
+    remove_keys(res_json)
+    res = json.dumps(res_json, indent=4, ensure_ascii=False)
     return res
 
 
-def write_file(data):
-    with open('source.json', 'w') as f:
+def remove_keys(result):
+    key_list = ["warningText"]
+    for key in key_list:
+        if key in result:
+            result.pop(key)
+
+
+def write_file(name, data):
+    with open('./json/{}.json'.format(name), 'w') as f:
         f.write(data)
 
 
+def main():
+    for template in source_list:
+        result = ""
+        url = template["url"]
+        print("开始同步 - {}".format(template["desc"]))
+        if template["allow_redirect"]:
+            url = get_redirect(url)
+        if template["from"] == "api":
+            result = get_conf(url)
+        elif template["from"] == "git":
+            result = get_git_conf(url)
+        if result:
+            write_file(template["name"], result)
+
+
 if __name__ == '__main__':
-    d = get_conf()
-    write_file(d)
+    main()
