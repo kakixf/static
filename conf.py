8798785c25fb3a2b4651c1d5eaac762eb7ff70fc
wangxiaofei <wangxiaofei@tianyancha.com>
2023-08-08 10:40:03 +0800

    update

--- conf.py
+++ conf.py
@@ -13,13 +13,13 @@ source_list = [
         "from": "api"
     }, {
         "name": "tvbox",
-        "url": "https://github.com/kakixf/TVBox-1/blob/main/tvbox.json",
+        "url": "https://github.com/2hacc/TVBox/blob/main/tvbox.json",
         "allow_redirect": False,
         "desc": "默认",
         "from": "git"
     }, {
         "name": "xiaoya",
-        "url": "https://github.com/kakixf/TVBox-1/blob/main/xiaoya.json",
+        "url": "https://github.com/2hacc/TVBox/blob/main/xiaoya.json",
         "allow_redirect": False,
         "desc": "小雅",
         "from": "git"
